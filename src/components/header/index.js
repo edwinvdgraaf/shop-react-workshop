ad628331ad47bc7647607daa30599ed2a9339556
Edwin van der Graaf <evandergraaf@bol.com>
2017-12-07 16:32:00 +0100

    Add basket to header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import BasketSvg  from './basket.svg';
 
 class Header extends Component {
   render() {
     return (
       <div className="header">
-        <div className="flex-left">
+        <div className="header__col">
           Bol.com
+        </div>
+        <div className="header__col">
+          <div className="header__basket">
+            <img src={BasketSvg} alt="Basket" width={16} height={16} /> Basket (0)
+          </div>
         </div>
       </div>
     );
