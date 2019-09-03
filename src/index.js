c600a9c5ee622bb0e52351350b230be41ec9595c
Edwin van der Graaf <evandergraaf@bol.com>
2019-09-03 16:16:43 +0200

    Clean service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/app';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
